ee576e867dc7e43637b322d75f531ed30e9f1b26
Brian Nelson <briannelson@Brians-MacBook-Air.local>
2024-08-14 14:53:25 -0400

    added ping command

--- src/commands.rs
+++ src/commands.rs
@@ -14,6 +14,6 @@ pub async fn age(
 
 #[poise::command(slash_command, prefix_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.reply(format!("Hello {}!", ctx.author().name)).await?;
+    ctx.reply(format!("Pong!")).await?;
     Ok(())
 }
